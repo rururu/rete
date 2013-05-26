75a103fc2864d54f318b9d2ebb829d85da893ecd
Ru <sorokin@oogis.ru>
2013-05-26 17:16:54 +0400

    added main-class to project and built jar and uberjar

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject rete "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Clojure RETE implementation"
+  :url "https://github.com/rururu/rete"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :main rete.core
   :dependencies [[org.clojure/clojure "1.5.1"]])
